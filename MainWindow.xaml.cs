939c300ef9ddb6c6b66c016d008543d6a5419c21
657130121@qq.com <zp1527902535>
2020-05-06 23:41:41 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using 记账.Pages;
+
+namespace 记账
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int selectedIndex = ((TabControl)sender).SelectedIndex;
+
+            switch (selectedIndex)
+            {
+                case 0:
+                    Page page = new Goods();
+                    content.Content = page;
+                    break;
+            }
+        }
+    }
+}
